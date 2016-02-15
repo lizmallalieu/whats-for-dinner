9ece9a90484982963d5f573616b8dcf164d8d4d0
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-02-15 15:23:16 -0800

    Implement Recipes collection with GET request to API

---
+++ js/collections/Recipes.js
@@ -0,0 +1,26 @@
+var Recipes = Backbone.Collection.extend({
+
+  model: RecipeEntry,
+
+  addRecipeEntries: function(ingredients) {
+
+    // modify ingredients string for URL
+    ingredients = ingredients.split(', ').join('%2');
+
+    $.get('GEThttps://spoonacular-recipe-food-nutrition-v1.p.mashape.com/
+      recipes/findByIngredients?ingredients=' + ingredients + '&limitLicense
+      =false&number=5&ranking=1', function(data) {
+
+        this.add({
+          id: data.id,
+          title: data.title,
+          image: data.image
+        });
+
+      }.bind(this);
+    
+    );
+
+  }
+
+});
