08e0ceef9957a7568ab6bb971c9394f8ec652e24
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-02-15 14:54:04 -0800

    Implement RecipeEntry model

---
+++ js/models/RecipeEntry.js
@@ -0,0 +1,11 @@
+var RecipeEntry = Backbone.Model.extend({
+
+  defaults: {
+    id: 0,
+    title: 'recipe',
+    image: undefined,
+    sourceUrl: // comes from GET recipe information route
+  },
+
+
+});
