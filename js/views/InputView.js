4a37afdd9143521a13550af16c4d5c640fb5f85e
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-02-15 15:39:12 -0800

    Implement InputView

---
+++ js/views/InputView.js
@@ -0,0 +1,37 @@
+var InputView = Backbone.View.extend({
+
+  el: '<input>',
+
+  events: {
+    'keydown': 'keyAction'
+  },
+
+  initialize: function() {
+    this.resetInput();
+    return this;
+  }
+
+  keyAction: function(e) {
+
+    var isEnterKey = (e.which === 13);
+
+    if(isEnterKey) {
+      this.collection.addRecipeEntries(this.$el.val());
+      this.resetInput();
+    }
+
+  },
+
+  resetInput: function() {
+    this.$el.attr({
+      placeholder: 'Enter ingredients separated by commas'
+    });
+    this.clearInput();
+  },
+
+  clearInput: function() {
+    this.$el.val('');
+  }
+
+
+});
